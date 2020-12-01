3ca5504d48be4379e675a856f267225ee613f70d
hamed alfahad <hamedalfahad@gmail.com>
2020-12-01 21:49:29 +0300

    initial commit

--- modules/System_GroupUsers/controllers/System_Group_Users.php
+++ modules/System_GroupUsers/controllers/System_Group_Users.php
@@ -1,1 +1,26 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class System_Group_Users extends Admin
+{
+
+    private $table_name           = 'portal_auth_groups';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+
+    ###################################################################
+    public function index()
+    {
+        $this->data['Page_Title'] = ' ادارة مجموعة المستخدمين ';
+
+        Layout_Admin($this->data);
+    }
+    ###################################################################
+
+
+
+}
